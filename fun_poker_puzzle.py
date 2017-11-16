27830d9cf7ed0c63b30a05096737b63f6294f008
Simon <s@simon0.com>
2017-11-16 12:15:50 -0500

    add fun_poker_puzzle and its unittest

---
+++ fun_poker_puzzle.py
@@ -0,0 +1,163 @@
+from operator import itemgetter
+
+HAND_SIZE = 5
+LOWEST_RANK = 10
+POKER_HAND_RANK = {1: "royal-flush", 2: "straight-flush", 3: "four-of-a-kind", 4: "full-house", 5: "flush",
+                   6: "straight",
+                   7: "three-of-a-kind", 8: "two-pair", 9: "pair", 10: "high-card"}
+
+
+def get_value_of_card(val):
+    if val == 'A':
+        return 14
+    elif val == 'K':
+        return 13
+    elif val == 'Q':
+        return 12
+    elif val == 'J':
+        return 11
+    elif val == 'T':
+        return 10
+    else:
+        return ord(val) - ord('0')
+
+
+def is_flush(sorted_hand):
+    for i in range(HAND_SIZE - 1):
+        if sorted_hand[i][1] != sorted_hand[i + 1][1]:
+            return False
+    return True
+
+
+def is_straight(sorted_hand):
+    for i in range(HAND_SIZE):
+        if sorted_hand[i][0] != sorted_hand[0][0] + i:
+            return False
+    return True
+
+
+def is_straight_flush(sorted_hand):
+    if is_straight(sorted_hand):
+        if is_flush(sorted_hand):
+            return True
+    return False
+
+
+def is_royal_flash(sorted_hand):
+    if is_straight_flush(sorted_hand):
+        if sorted_hand[0][0] == 10:
+            return True
+    return False
+
+
+def is_four_of_a_kind(sorted_hand):
+    if sorted_hand[0][0] == sorted_hand[3][0] or sorted_hand[1][0] == sorted_hand[4][0]:
+        return True
+    else:
+        return False
+
+
+def is_full_house(sorted_hand):
+    if sorted_hand[0][0] == sorted_hand[2][0] and sorted_hand[3][0] == sorted_hand[4][0]:
+        return True
+    if sorted_hand[0][0] == sorted_hand[1][0] and sorted_hand[2][0] == sorted_hand[4][0]:
+        return True
+    return False
+
+
+def is_three_of_a_kind(sorted_hand):
+    for i in range(HAND_SIZE - 2):
+        if sorted_hand[i][0] == sorted_hand[i + 2][0]:
+            return True
+    return False
+
+
+def get_different_pair_count(sorted_hand):
+    pair_found = 0
+    card_dict = {}
+    for i in sorted_hand:
+        if i[0] not in card_dict:
+            card_dict[i[0]] = 1
+        else:
+            card_dict[i[0]] += 1
+    for i in card_dict:
+        if card_dict[i] == 2:
+            pair_found += 1
+    return pair_found
+
+
+def is_two_pair(sorted_hand):
+    if get_different_pair_count(sorted_hand) == 2:
+        return True
+    else:
+        return False
+
+
+def is_pair(sorted_hand):
+    if get_different_pair_count(sorted_hand) == 1:
+        return True
+    else:
+        return False
+
+
+def get_current_hand(hand):
+    sorted_hand = sorted(hand, key=itemgetter(0))
+    if is_royal_flash(sorted_hand):
+        return 1
+    if is_straight_flush(sorted_hand):
+        return 2
+    if is_four_of_a_kind(sorted_hand):
+        return 3
+    if is_full_house(sorted_hand):
+        return 4
+    if is_flush(sorted_hand):
+        return 5
+    if is_straight(sorted_hand):
+        return 6
+    if is_three_of_a_kind(sorted_hand):
+        return 7
+    if is_two_pair(sorted_hand):
+        return 8
+    if is_pair(sorted_hand):
+        return 9
+    return 10
+
+
+def get_rank(data, to_drop, hand_pointer, deck_pointer, hand):
+    if hand_pointer == HAND_SIZE:
+        return get_current_hand(hand)
+    ret = LOWEST_RANK
+    if hand_pointer + to_drop < HAND_SIZE:
+        hand.append(data[hand_pointer])
+        ret = min(ret, get_rank(data, to_drop, hand_pointer + 1, deck_pointer, hand))
+        hand.pop(-1)
+    if to_drop > 0:
+        hand.append(data[HAND_SIZE + deck_pointer])
+        ret = min(ret, get_rank(data, to_drop - 1, hand_pointer + 1, deck_pointer + 1, hand))
+        hand.pop(-1)
+    return ret
+
+
+def main(data):
+    rendered_data = []
+    for i in data:
+        rendered_data.append((get_value_of_card(i[0]), i[1]))
+
+    ans = LOWEST_RANK
+    for i in range(HAND_SIZE + 1):
+        ans = min(ans, get_rank(rendered_data, i, 0, 0, []))
+    ret = 'hand: '
+    # print('hand: ',end='')
+    for i in range(HAND_SIZE):
+        ret += data[i] + ' '
+    ret += 'deck: '
+    for i in range(HAND_SIZE):
+        ret += data[HAND_SIZE + i] + ' '
+    ret += 'highest: ' + POKER_HAND_RANK[ans]
+
+    return ret
+
+
+if __name__ == '__main__':
+    original_data = input().strip().split()
+    print(main(original_data))
---
+++ fun_poker_puzzle_unittest.py
@@ -0,0 +1,165 @@
+import unittest
+import re
+from fun_poker_puzzle import *
+
+
+class TestPuzzle(unittest.TestCase):
+    def test_royal_flush_1(self):
+        data = "7D 8D 9D TD JD AD QD KD 5D 6D".split()
+        ans = main(data)
+        print("expect royal-flush ", ans)
+        self.assertRegex(ans, re.compile('.*highest: royal-flush'))
+
+    def test_royal_flush_2(self):
+        data = "TD JD AD QD KD 7D 8D 9D 5D 6D".split()
+        ans = main(data)
+        print("expect royal-flush ", ans)
+        self.assertRegex(ans, re.compile('.*highest: royal-flush'))
+
+    def test_straight_flush_1(self):
+        data = "3H 5H 2H 9H KH 6H 4H 4D AD 2D".split()
+        ans = main(data)
+        print("expect straight-flush ", ans)
+        self.assertRegex(ans, re.compile('.*highest: straight-flush'))
+
+    def test_straight_flush_2(self):
+        data = "9H KH AD 2D 3H 5H 2H 6H 4H 4D".split()
+        ans = main(data)
+        print("expect straight-flush ", ans)
+        self.assertRegex(ans, re.compile('.*highest: straight-flush'))
+
+    def test_four_of_a_kind_1(self):
+        data = "9H KH AD 2D 3H 9D 9S 9C 4H 4D".split()
+        ans = main(data)
+        print("expect four-of-a-kind ", ans)
+        self.assertRegex(ans, re.compile('.*highest: four-of-a-kind'))
+
+    def test_four_of_a_kind_2(self):
+        data = "KH AD 2D 3H 5H 2H 9H 9D 9S 9C".split()
+        ans = main(data)
+        print("expect four-of-a-kind ", ans)
+        self.assertRegex(ans, re.compile('.*highest: four-of-a-kind'))
+
+    def test_full_house_1(self):
+        data = "9H KH AD 2D 3H 9D 9S 2H 3C 4D".split()
+        ans = main(data)
+        print("expect full-house ", ans)
+        self.assertRegex(ans, re.compile('.*highest: full-house'))
+
+    def test_full_house_2(self):
+        data = "9H 9D 9S 2H 2D KH AD 3H 3C 4D".split()
+        ans = main(data)
+        print("expect full-house ", ans)
+        self.assertRegex(ans, re.compile('.*highest: full-house'))
+
+    def test_flush_1(self):
+        data = "9H 4D 9S 2H 2D KH AH 3H 3C 4D".split()
+        ans = main(data)
+        print("expect flush ", ans)
+        self.assertRegex(ans, re.compile('.*highest: flush'))
+
+    def test_flush_2(self):
+        data = "4D 9S 2H 2D KH AH 3H 9H 3C 4D".split()
+        ans = main(data)
+        print("expect flush ", ans)
+        self.assertRegex(ans, re.compile('.*highest: flush'))
+
+    def test_straight_1(self):
+        data = "9H 8D 9S QH 2D TH JD 3H 3C 4D".split()
+        ans = main(data)
+        print("expect straight ", ans)
+        self.assertRegex(ans, re.compile('.*highest: straight'))
+
+    def test_straight_2(self):
+        data = "9H 8D 9S QH TD TD JH 3H 3C 4D".split()
+        ans = main(data)
+        print("expect straight ", ans)
+        self.assertRegex(ans, re.compile('.*highest: straight'))
+
+    def test_three_of_a_kind_1(self):
+        data = "9H 8D 9S 6H 2D AH JD 9C 3H 9D".split()
+        ans = main(data)
+        print("expect three-of-a-kind ", ans)
+        self.assertRegex(ans, re.compile('.*highest: three-of-a-kind'))
+
+    def test_three_of_a_kind_2(self):
+        data = "9H 8D 4S 7H 2D AH JD 2H 2C 9D".split()
+        ans = main(data)
+        print("expect three-of-a-kind ", ans)
+        self.assertRegex(ans, re.compile('.*highest: three-of-a-kind'))
+
+    def test_two_pair_1(self):
+        data = "9H 8D 9S 6H 2D 8H AH JD 9C 9D".split()
+        ans = main(data)
+        print("expect two-pair ", ans)
+        self.assertRegex(ans, re.compile('.*highest: two-pair'))
+
+    def test_two_pair_2(self):
+        data = "9H 3D 9S 6H 2D 8H AH 8D 9C 9D".split()
+        ans = main(data)
+        print("expect two-pair ", ans)
+        self.assertRegex(ans, re.compile('.*highest: two-pair'))
+
+    def test_pair_1(self):
+        data = "9H 8D 9S 6H 2D AH JD 3H 9C 9D".split()
+        ans = main(data)
+        print("expect pair ", ans)
+        self.assertRegex(ans, re.compile('.*highest: pair'))
+
+    def test_pair_2(self):
+        data = "9H 8D 7S 6H 2D AH JD 3H 9C 9D".split()
+        ans = main(data)
+        print("expect pair ", ans)
+        self.assertRegex(ans, re.compile('.*highest: pair'))
+
+    def test_high_card_1(self):
+        data = "9H 8D 7S 6H 2D AH JD 3H 5C 9D".split()
+        ans = main(data)
+        print("expect high-card ", ans)
+        self.assertRegex(ans, re.compile('.*highest: high-card'))
+
+    def test_high_card_2(self):
+        data = "9H 8D 7S 6H 2D AH JD 3H TC 9D".split()
+        ans = main(data)
+        print("expect high-card ", ans)
+        self.assertRegex(ans, re.compile('.*highest: high-card'))
+
+    def test_total_drop_0(self):
+        data = "TD JD AD QD KD 1D 3D 2D 4D 5D".split()
+        ans = main(data)
+        print("expect royal-flush ", ans)
+        self.assertRegex(ans, re.compile('.*highest: royal-flush'))
+
+    def test_total_drop_1(self):
+        data = "TD JD AD QD 1D KD 3D 2D 4D 5D".split()
+        ans = main(data)
+        print("expect royal-flush ", ans)
+        self.assertRegex(ans, re.compile('.*highest: royal-flush'))
+
+    def test_total_drop_2(self):
+        data = "TD 1D JD 4D AD QD KD 3D 2D 5D".split()
+        ans = main(data)
+        print("expect royal-flush ", ans)
+        self.assertRegex(ans, re.compile('.*highest: royal-flush'))
+
+    def test_total_drop_3(self):
+        data = "3D TD 1D JD 4D AD QD KD 2D 5D".split()
+        ans = main(data)
+        print("expect royal-flush ", ans)
+        self.assertRegex(ans, re.compile('.*highest: royal-flush'))
+
+    def test_total_drop_4(self):
+        data = "3D 2D TD 1D 4D JD AD QD KD 5D".split()
+        ans = main(data)
+        print("expect royal-flush ", ans)
+        self.assertRegex(ans, re.compile('.*highest: royal-flush'))
+
+    def test_total_drop_5(self):
+        data = "3D 2D 1D 4D 5D TD JD AD QD KD".split()
+        ans = main(data)
+        print("expect royal-flush ", ans)
+        self.assertRegex(ans, re.compile('.*highest: royal-flush'))
+
+
+if __name__ == '__main__':
+    unittest.main()
